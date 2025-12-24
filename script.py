6f452b191b53f31b2b7a9c1bf7bf8450a03b25ed
SheikhNazir <73103801+naazie@users.noreply.github.com>
2025-12-24 20:16:21 +0530

    Add initial Python script

---
+++ script.py
@@ -0,0 +1,20 @@
+
+a = "Text2"
+c = "s"
+b = 2356
+is_active = True
+username = "root"
+password = "1234567"
+if __name__ == "__main__":
+    print("Hello Python")
+    print(type(is_active))
+    print(b)
+
+    # Taking User Inputs
+    name = input("Enter your name: ")
+    print(name)
+    age = input("Enter your age: ")
+    print(age)
+
+
+
