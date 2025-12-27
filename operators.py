22e67d196ab2be4dce20b6040d7ae161a7f84d3d
SheikhNazir <73103801+naazie@users.noreply.github.com>
2025-12-27 18:51:20 +0530

    Operators in Python

---
+++ operators.py
@@ -0,0 +1,24 @@
+if __name__ == "__main__":
+#Operators: +,-,*,/,%
+    x = 10
+    y = 3
+    z= 3
+    print(x+z)
+    print(x-z)
+    print(x*z)
+    print(x/z)
+    print(x%z)
+    #Comparison Operators(=,>,<,!=)
+
+    print(x>z)
+    print(x<z)
+    print(x != z)
+    print(y == z)
+
+    # IF ELSE Statements
+    if x > 9:
+        print("x is greater than 10")
+    elif x == 10:
+        print("x is equal to 10")
+    else:
+        print("x is less than to 10")
